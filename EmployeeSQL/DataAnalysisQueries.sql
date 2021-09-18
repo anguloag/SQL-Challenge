a1edcee7311a79d8ae7eb73e1de9a801306b2139
Angela <anguloag@vcu.edu>
2021-09-18 00:29:14 -0400

    edited sql code, finished jupyter notebook

--- EmployeeSQL/DataAnalysisQueries.sql
+++ EmployeeSQL/DataAnalysisQueries.sql
@@ -2,60 +2,60 @@
 
 --1) List the employee #, last name, first name, sex, & salary for each employee
 ----have to merge info from employees and salaries tables
-SELECT e.EmpNum, e.LastName, e.FirstName, e.Sex, s.Salary
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
-INNER JOIN salaries AS s ON e.EmpNum=s.EmpNum;
+INNER JOIN salaries AS s ON e.emp_no=s.emp_no;
 
 --2) List the first name, last name, and hire date for those hired in 1986
 ----have to limit data from employees table to dates between 1/1/86 and 12/31/86 
-SELECT FirstName, LastName, HireDate
+SELECT first_name, last_name, hire_date
 FROM employees
-WHERE HireDate BETWEEN '1986-01-01' AND '1986-12-31';
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 --3) List the dept number, dept name, employee number, last name & 1st name for each manager
 ----have to take data from dept_managers, departments, and employees tables with merges
-SELECT dm.DeptNum, d.DeptName, dm.EmpNum, e.LastName, e.FirstName
-FROM dept_managers AS dm
-INNER JOIN departments AS d ON dm.DeptNum=d.DeptNum
-INNER JOIN employees AS e ON dm.EmpNum=e.EmpNum;
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+INNER JOIN departments AS d ON dm.dept_no=d.dept_no
+INNER JOIN employees AS e ON dm.emp_no=e.emp_no;
 
 --4) List the employee number, last name, first name, & dept name for each employee
 ----to get department name that corresponds to each employee,
 ----you have to merge employees table with dept_employees based on employee number
 ----to then merge dept_employees with departments table based dept number
-SELECT e.EmpNum, e.LastName, e.FirstName, d.DeptName
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
-INNER JOIN dept_employees AS de ON e.EmpNum=de.EmpNum
-INNER JOIN departments AS d ON de.DeptNum=d.DeptNum;
+INNER JOIN dept_emp AS de ON e.emp_no=de.emp_no
+INNER JOIN departments AS d ON de.dept_no=d.dept_no;
 
 --5) List 1st name, last name, & sex of those with 1st name Hercules & last name starts w/ B 
-SELECT FirstName, LastName, Sex 
+SELECT first_name, last_name, sex 
 FROM employees 
-WHERE FirstName = 'Hercules' AND LastName LIKE 'B%';
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 --6) List employee number, last name, 1st name, & dept name of all Sales Dept employees
 ----repeat process for #4
 ----then limit data to those with DeptName 'Sales' in departments table
-SELECT e.EmpNum, e.LastName, e.FirstName, d.DeptName
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
-INNER JOIN dept_employees AS de ON e.EmpNum=de.EmpNum
-INNER JOIN departments AS d ON de.DeptNum=d.DeptNum
-WHERE d.DeptName = 'Sales';
+INNER JOIN dept_emp AS de ON e.emp_no=de.emp_no
+INNER JOIN departments AS d ON de.dept_no=d.dept_no
+WHERE d.dept_name = 'Sales';
 
 --7) List employee number, last name, 1st name, & dept name of Sales and Dev Dept employees
 ----repeat process for #4
 ----then limit data to those with DeptName 'Sales' OR 'Development' in departments table
-SELECT e.EmpNum, e.LastName, e.FirstName, d.DeptName
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
-INNER JOIN dept_employees AS de ON e.EmpNum=de.EmpNum
-INNER JOIN departments AS d ON de.DeptNum=d.DeptNum
-WHERE d.DeptName = 'Sales' OR d.DeptName = 'Development';
+INNER JOIN dept_emp AS de ON e.emp_no=de.emp_no
+INNER JOIN departments AS d ON de.dept_no=d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 --8) List the frequency count of employee last names in descending order
 ----use data from employees table to get count of last names
 ----have to use a group by function (group by last name),
 ----and order by funciton (by the descending count of last names)
-SELECT LastName, COUNT(LastName)
+SELECT last_name, COUNT(last_name)
 FROM employees
-GROUP BY LastName
-ORDER BY COUNT(LastName) DESC;
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
--- EmployeeSQL/TableSchemata.sql
+++ EmployeeSQL/TableSchemata.sql
@@ -2,59 +2,61 @@
 
 --Create departments table
 CREATE TABLE departments (
-	DeptNum VARCHAR(10) PRIMARY KEY,
-	DeptName VARCHAR);
+	dept_no VARCHAR(10) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR NOT NULL);
 --Check imported data
 SELECT * FROM departments;
 
 --Create titles table
 CREATE TABLE titles (
-	TitleID VARCHAR(10) PRIMARY KEY,
-	TitleName VARCHAR);
+	title_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	title VARCHAR NOT NULL);
 --Check imported data
 SELECT * FROM titles;
 
 --Create employees table
 CREATE TABLE employees (
-	EmpNum INT PRIMARY KEY,
-	EmpTitleID VARCHAR(10),
-	BirthDate DATE,
-	FirstName VARCHAR,
-	LastName VARCHAR,
-	Sex VARCHAR,
-	HireDate DATE,
-	FOREIGN KEY (EmpTitleID) REFERENCES titles(TitleID));
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR(10) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id));
 --Check imported data
 SELECT * FROM employees;
 
 --Create dept_employees table
-CREATE TABLE dept_employees (
-	EmpNum INT,
-	DeptNum VARCHAR(10),
-	FOREIGN KEY (EmpNum) REFERENCES employees(EmpNum),
-	FOREIGN KEY (DeptNum) REFERENCES departments(DeptNum));
---Check imported data
-SELECT * FROM dept_employees;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no));
 --Add composite key to table
-ALTER TABLE dept_employees 
-ADD PRIMARY KEY (EmpNum, DeptNum); 
+ALTER TABLE dept_emp 
+ADD PRIMARY KEY (emp_no, dept_no); 
+--Check imported data
+SELECT * FROM dept_emp;
 
 --Create dept_managers table
-CREATE TABLE dept_managers (
-	DeptNum VARCHAR(10),
-	EmpNum INT,
-	FOREIGN KEY (DeptNum) REFERENCES departments(DeptNum),
-	FOREIGN KEY (EmpNum) REFERENCES employees(EmpNum));
---Check imported data
-SELECT * FROM dept_managers;
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
 --Add composite key to table
-ALTER TABLE dept_managers
-ADD PRIMARY KEY (DeptNum, EmpNum); 
+ALTER TABLE dept_manager
+ADD PRIMARY KEY (dept_no, emp_no); 
+--Check imported data
+SELECT * FROM dept_manager;
 
 --Create salaries table
 CREATE TABLE salaries (
-	EmpNum INT,
-	Salary INT,
-	FOREIGN KEY (EmpNum) REFERENCES employees(EmpNum));
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
+ALTER TABLE salaries
+ADD PRIMARY KEY (emp_no);
 --Check imported data
 SELECT * FROM salaries;
